674dbf1606263394daf024b89e2524698dab6da8
Ruchie Rubia <ruchier@codev.com>
2019-09-17 23:31:37 +0800

    Scaffold components and module, setup json server, setup lazy loading routes, create initail componet views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule, StoreModule.forRoot({})
+    BrowserModule, StoreModule.forRoot({}), AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
