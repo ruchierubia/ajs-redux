aa6b7562235a7ac52d87dd57388e9e0b69e9da52
ruchierubia <ruch.rubi@gmail.com>
2019-09-30 14:55:38 +0800

    Add action creators

--- src/app/customers/state/customer.actions.ts
+++ src/app/customers/state/customer.actions.ts
@@ -38,4 +38,76 @@ export class LoadCustomersFail implements Action {
     constructor(public  payload: string) {}
 }
 
+export class LoadCustomer implements Action {
+  readonly type = CustomerActionTypes.LOAD_CUSTOMER;
+
+  constructor(public payload: number) {}
+}
+
+export class LoadCustomerSuccess implements Action {
+  readonly type = CustomerActionTypes.LOAD_CUSTOMER_SUCCESS;
+
+  constructor(public payload: Customer) {}
+}
+
+export class LoadCustomerFail implements Action {
+  readonly type = CustomerActionTypes.LOAD_CUSTOMER_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export class CreateCustomer implements Action {
+  readonly type = CustomerActionTypes.CREATE_CUSTOMER;
+
+  constructor(public payload: Customer) {}
+}
+
+export class CreateCustomerSuccess implements Action {
+  readonly type = CustomerActionTypes.CREATE_CUSTOMER_SUCCESS;
+
+  constructor(public payload: Customer) {}
+}
+
+export class CreateCustomerFail implements Action {
+  readonly type = CustomerActionTypes.CREATE_CUSTOMER_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export class UpdateCustomer implements Action {
+  readonly type = CustomerActionTypes.UPDATE_CUSTOMER;
+
+  constructor(public payload: Customer) {}
+}
+
+export class UpdateCustomerSuccess implements Action {
+  readonly type = CustomerActionTypes.UPDATE_CUSTOMER_SUCCESS;
+
+  constructor(public payload: Update<Customer>) {}
+}
+
+export class UpdateCustomerFail implements Action {
+  readonly type = CustomerActionTypes.UPDATE_CUSTOMER_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export class DeleteCustomer implements Action {
+  readonly type = CustomerActionTypes.DELETE_CUSTOMER;
+
+  constructor(public payload: number) {}
+}
+
+export class DeleteCustomerSuccess implements Action {
+  readonly type = CustomerActionTypes.DELETE_CUSTOMER_SUCCESS;
+
+  constructor(public payload: number) {}
+}
+
+export class DeleteCustomerFail implements Action {
+  readonly type = CustomerActionTypes.DELETE_CUSTOMER_FAIL;
+
+  constructor(public payload: string) {}
+}
+
 export type Action = LoadCustomers | LoadCustomersSuccess | LoadCustomersFail;
