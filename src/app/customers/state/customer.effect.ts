948b5401d8316dd4d4066146057c13e4a969f4bc
ruchierubia <ruch.rubi@gmail.com>
2019-09-30 15:05:42 +0800

    8 add all effects

--- src/app/customers/state/customer.effect.ts
+++ src/app/customers/state/customer.effect.ts
@@ -13,7 +13,7 @@ import { Customer } from '../customer.model';
 export class CustomerEffect {
   constructor(
     private actions$: Actions,
-    private customersService: CustomerService
+    private customerService: CustomerService
   ) {}
 
   @Effect()
@@ -22,12 +22,79 @@ export class CustomerEffect {
       customerActions.CustomerActionTypes.LOAD_CUSTOMERS
     ),
     mergeMap(( action: customerActions.LoadCustomers) =>
-      this.customersService.getCustomers().pipe(
+      this.customerService.getCustomers().pipe(
         map(
           (customers: Customer[]) =>
             new customerActions.LoadCustomersSuccess(customers)
         ),
         catchError( err => of(new customerActions.LoadCustomersFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  loadCustomer$: Observable<Action> = this.actions$.pipe(
+    ofType<customerActions.LoadCustomer>(
+      customerActions.CustomerActionTypes.LOAD_CUSTOMER
+    ),
+    mergeMap((action: customerActions.LoadCustomer) =>
+      this.customerService.getCustomerById(action.payload).pipe(
+        map(
+          (customer: Customer) =>
+            new customerActions.LoadCustomerSuccess(customer)
+        ),
+        catchError(err => of(new customerActions.LoadCustomerFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  createCustomer$: Observable<Action> = this.actions$.pipe(
+    ofType<customerActions.CreateCustomer>(
+      customerActions.CustomerActionTypes.CREATE_CUSTOMER
+    ),
+    map((action: customerActions.CreateCustomer) => action.payload),
+    mergeMap((customer: Customer) =>
+      this.customerService.createCustomer(customer).pipe(
+        map(
+          (newCustomer: Customer) =>
+            new customerActions.CreateCustomerSuccess(newCustomer)
+        ),
+        catchError(err => of(new customerActions.CreateCustomerFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  updateCustomer$: Observable<Action> = this.actions$.pipe(
+    ofType<customerActions.UpdateCustomer>(
+      customerActions.CustomerActionTypes.UPDATE_CUSTOMER
+    ),
+    map((action: customerActions.UpdateCustomer) => action.payload),
+    mergeMap((customer: Customer) =>
+      this.customerService.updateCustomer(customer).pipe(
+        map(
+          (updateCustomer: Customer) =>
+            new customerActions.UpdateCustomerSuccess({
+              id: updateCustomer.id,
+              changes: updateCustomer
+            })
+        ),
+        catchError(err => of(new customerActions.UpdateCustomerFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  deleteCustomer$: Observable<Action> = this.actions$.pipe(
+    ofType<customerActions.DeleteCustomer>(
+      customerActions.CustomerActionTypes.DELETE_CUSTOMER
+    ),
+    map((action: customerActions.DeleteCustomer) => action.payload),
+    mergeMap((id: number) =>
+      this.customerService.deleteCustomer(id).pipe(
+        map(() => new customerActions.DeleteCustomerSuccess(id)),
+        catchError(err => of(new customerActions.DeleteCustomerFail(err)))
       )
     )
   );
